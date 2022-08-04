963f598017edb6a08a91e057ccf6676b7b7e9eed
romaricano <58915209+romaricano@users.noreply.github.com>
2022-08-04 13:19:03 +0000

    update

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,15 @@ class Axe extends Piece {
 }
 
 class Piston {
-    Tete tete;
-    Jupe jupe;
-    Axe axe;
+    Piece tete;
+    Piece jupe;
+    Piece axe;
+
+    public Piston(Piece tete, Piece jupe, Piece axe) {
+        this.tete = tete;
+        this.jupe = jupe;
+        this.axe = axe;
+    }
 }
 
 class Machine {
@@ -33,6 +39,48 @@ class Machine {
     public Machine(int tempsTraitementParPiece) {
         this.tempsTraitementParPiece = tempsTraitementParPiece;
     }
+
+    public int getTempsTraitementParPiece() {
+        return tempsTraitementParPiece;
+    }
+}
+
+class MT extends Machine {
+
+    public MT(int tempsTraitementParPiece) {
+        super(tempsTraitementParPiece);
+    }
+
+    public Piece traiter() {
+
+    }
+}
+
+class MP extends Machine {
+
+    public static int nombrePistonsAssembles = 0;
+
+    public int tempsTotalDeTraitementEnSeconde() {
+        return getTempsTraitementParPiece() * nombrePistonsAssembles;
+    }
+
+    public MP(int tempsTraitementParPiece) {
+        super(tempsTraitementParPiece);
+    }
+
+    public Piston assembler(Piece tete, Piece jupe, Piece axe) {
+        if(tete == null || jupe == null || axe == null) {
+            return null;
+        }
+        nombrePistonsAssembles++;
+        return new Piston(tete, jupe, axe);
+    }
+}
+
+class ChaineDeProduction {
+    MP mp;
+
+
 }
 
 
